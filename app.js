6ee9ffd84bc89d1572d009dcb28f4a25297785f6
John-Rashta <john_rashta@outlook.com>
2024-12-18 15:32:43 +0000

    feat: basic files

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const path = require("node:path");
+
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+app.use(express.urlencoded({ extended: true }));
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+
+
+app.listen(8080);
