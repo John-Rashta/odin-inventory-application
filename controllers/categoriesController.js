31ab0a8db3754e5642205de7a242237f54e46d0f
John-Rashta <john_rashta@outlook.com>
2024-12-19 20:56:47 +0000

    feat: add validation

--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -1,4 +1,4 @@
-const { body, validationResult, query } = require("express-validator");
+const { body, validationResult, param } = require("express-validator");
 const db = require("../db/queries");
 
 exports.showAllCategories = async (req, res) => {
@@ -6,41 +6,88 @@ exports.showAllCategories = async (req, res) => {
     res.render("categories", {categories: newData});
 }
 
-exports.showCategory = async (req, res) => {
-    const searchId = req.params.categoryid;
-    const newData = await db.getAllItemsInCategory(searchId);
-    const catData = await db.searchForCategory(searchId);
-    res.render("category", {items: newData, category: catData[0]});
-}
+const validateName = [
+    body("category_name").trim()
+        .isAlpha().withMessage("Must only contain letters.")
+        .isLength({min: 1, max: 255}).withMessage("Max 255 characters."),
+  ];
+  
+const validateId = [
+    param("categoryid").trim()
+        .toInt().isInt(),
+];
 
-exports.startUpdateCategory = async (req, res) => {
-    const searchId = req.params.categoryid;
-    const newData = await db.searchForCategory(searchId);
-    res.render("updateCategory", {category: newData[0]});
-    
-}
+exports.showCategory = [
+    validateId,
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).send("ERROR");
+        }
+        const searchId = req.params.categoryid;
+        const newData = await db.getAllItemsInCategory(searchId);
+        const catData = await db.searchForCategory(searchId);
+        res.render("category", {items: newData, category: catData[0]});
+    }
+];
 
-exports.updateCategory = async (req, res) => {
-    const newUpdate = req.body;
-    const searchId = req.params.categoryid;
-    await db.updateCategory(searchId, newUpdate.category_name);
-    res.redirect("/");
-}
+exports.startUpdateCategory = [
+    validateId,
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).send("ERROR");
+        }
+        const searchId = req.params.categoryid;
+        const newData = await db.searchForCategory(searchId);
+        res.render("updateCategory", {category: newData[0]});
+        
+    }
+];
 
-exports.deleteCategory = async (req, res) => {
-    const searchId = req.params.categoryid;
-    await db.deleteCategory(searchId);
-    res.redirect("/");
-}
+exports.updateCategory = [
+    validateId.concat(validateName),
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).send("ERROR");
+        }
+        const newUpdate = req.body;
+        const searchId = req.params.categoryid;
+        await db.updateCategory(searchId, newUpdate.category_name);
+        res.redirect("/");
+    }
+];
+
+exports.deleteCategory = [
+    validateId,
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).send("ERROR");
+        }
+        const searchId = req.params.categoryid;
+        await db.deleteCategory(searchId);
+        res.redirect("/");
+    }
+
+];
 
 exports.startCreateCategory = async (req, res) => {
     res.render("createCategory");
 }
 
-exports.createCategory = async (req, res) => {
-    const newCreate = req.body;
-    await db.insertCategory(newCreate.category_name);
-    res.redirect("/");
-}
+exports.createCategory = [
+    validateName,
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).send("ERROR");
+        }
+        const newCreate = req.body;
+        await db.insertCategory(newCreate.category_name);
+        res.redirect("/");
+    }
+];
 
 
--- controllers/itemsController.js
+++ controllers/itemsController.js
@@ -1,60 +1,114 @@
-const { body, validationResult, query } = require("express-validator");
+const { body, validationResult, query, param } = require("express-validator");
 const db = require("../db/queries");
 
-exports.showItem = async (req, res) => {
-    const searchId = req.params.itemid;
-    const newData = await db.searchForItem(searchId);
-    res.render("item", {item: newData[0]});
-}
+const validateForm = [
+    body("item_name").trim()
+        .isAlpha().withMessage("Must only contain letters.")
+        .isLength({min: 1, max: 255}).withMessage("Max 255 characters."),
+    body("price").trim()
+        .isFloat().withMessage("Must be a number. Examples: 25, 15.45"),
+    body("measure").trim()
+        .isAlphanumeric().withMessage("Must only contain letters and/or numbers.")
+        .isLength({min: 1, max: 100}).withMessage("Max 100 characters."),
+    body("categories").isInt(),
+  ];
+  
+const validateId = [
+    param("itemid").trim()
+        .toInt().isInt(),
+];
 
-exports.startUpdateItem = async (req, res) => {
-    const searchId = req.params.itemid;
-    const allCategories = await db.getAllCategories();
-    const newData = await db.searchForItem(searchId);
-    res.render("updateItem", {categories: allCategories, item: newData[0]});
-    
-}
-
-exports.updateItem = async (req, res) => {
-    const newUpdate = req.body;
-    const searchId = req.params.itemid;
-    await db.updateItem(searchId, newUpdate.item_name, newUpdate.price, newUpdate.measure);
-    await db.deleteCategoriesFromItem(searchId);
-    if (newUpdate.categories) {
-        if (Array.isArray(newUpdate.categories)) {
-            await Promise.all(newUpdate.categories.map(async (category) => {
-                await db.addCategoryOnItem(searchId, category);
-            }))
-        } else {
-            await db.addCategoryOnItem(searchId, newUpdate.categories);
+exports.showItem = [
+    validateId,
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).send("ERROR");
         }
+        const searchId = req.params.itemid;
+        const newData = await db.searchForItem(searchId);
+        res.render("item", {item: newData[0]});
     }
-    res.redirect("/");
-    
-}
 
-exports.deleteItem = async (req, res) => {
-    const searchId = req.params.itemid;
-    await db.deleteItem(searchId);
-    res.redirect("/");
-}
+];
+
+exports.startUpdateItem = [
+    validateId,
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).send("ERROR");
+        }
+        const searchId = req.params.itemid;
+        const allCategories = await db.getAllCategories();
+        const newData = await db.searchForItem(searchId);
+        res.render("updateItem", {categories: allCategories, item: newData[0]});
+        
+    }
+];
+
+exports.updateItem = [
+    validateId.concat(validateForm),
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).send("ERROR");
+        }
+        const newUpdate = req.body;
+        const searchId = req.params.itemid;
+        await db.updateItem(searchId, newUpdate.item_name, newUpdate.price, newUpdate.measure);
+        await db.deleteCategoriesFromItem(searchId);
+        if (newUpdate.categories) {
+            if (Array.isArray(newUpdate.categories)) {
+                await Promise.all(newUpdate.categories.map(async (category) => {
+                    await db.addCategoryOnItem(searchId, category);
+                }))
+            } else {
+                await db.addCategoryOnItem(searchId, newUpdate.categories);
+            }
+        }
+        res.redirect("/");
+        
+    }
+];
+
+exports.deleteItem = [
+    validateId,
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).send("ERROR");
+        }
+        const searchId = req.params.itemid;
+        await db.deleteItem(searchId);
+        res.redirect("/");
+    }
+];
 
 exports.startCreateItem = async (req, res) => {
     const categories = await db.getAllCategories();
     res.render("create" , {categories});
-}
+};
 
-exports.createItem = async (req, res) => {
-    const newCreate = req.body;
-    const [newId] = await db.insertItem(newCreate.item_name, newCreate.price, newCreate.measure);
-    if (newCreate.categories) {
-        if (Array.isArray(newCreate.categories)) {
-            await Promise.all(newCreate.categories.map(async (category) => {
-                await db.addCategoryOnItem(newId.id, category);
-            }))
-        } else {
-            await db.addCategoryOnItem(newId.id, newCreate.categories);
+exports.createItem = [
+    validateId.concat(validateForm),
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).send("ERROR");
         }
+        const newCreate = req.body;
+        const [newId] = await db.insertItem(newCreate.item_name, newCreate.price, newCreate.measure);
+        if (newCreate.categories) {
+            if (Array.isArray(newCreate.categories)) {
+                await Promise.all(newCreate.categories.map(async (category) => {
+                    await db.addCategoryOnItem(newId.id, category);
+                }))
+            } else {
+                await db.addCategoryOnItem(newId.id, newCreate.categories);
+            }
+        }
+        res.redirect("/");
     }
-    res.redirect("/");
-}
+
+];
